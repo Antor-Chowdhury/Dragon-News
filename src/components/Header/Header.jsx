7fb8ef9d3c03720eb5d630423fe4f2e38ec7bbdf
Antor-Chowdhury <antor1347@gmail.com>
2025-11-07 23:09:24 +0600

    added date

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import logo from "../../assets/logo.png";
+import { format } from "date-fns";
 
 const Header = () => {
   return (
     <div className="flex justify-center flex-col items-center gap-5 mt-12">
       <img src={logo} alt="" />
-      <p className="text-lg dark-3">Journalism Without Fear or Favour</p>
+      <p className="text-lg text-accent">Journalism Without Fear or Favour</p>
+      <p className="font-medium text-xl -mt-2.5 text-accent">
+        <span className="text-[#403f3f]">{format(new Date(), "EEEE")}</span>,{" "}
+        {format(new Date(), "MMMM MM, yyyy")}
+      </p>
     </div>
   );
 };
