ce291e76afc0dc40533f983251a7691e20fba4e8
Antor-Chowdhury <antor1347@gmail.com>
2025-11-09 02:09:37 +0600

    added categorywise news functionality

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,11 +1,34 @@
-import React from "react";
-import { useParams } from "react-router";
+import React, { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router";
 
 const CategoryNews = () => {
   const { id } = useParams();
-  console.log(id);
 
-  return <div>Category News - {id}</div>;
+  const data = useLoaderData();
+
+  const [categoryNews, setCategoryNews] = useState([]);
+
+  //   console.log(id, data);
+
+  //   UseEffect to differentiate the data category.
+  useEffect(() => {
+    if (id == "0") {
+      setCategoryNews(data);
+    } else if (id == "1") {
+      const filteredNews = data.filter(
+        (news) => news.others.is_today_pick == true
+      );
+      setCategoryNews(filteredNews);
+    } else {
+      const filteredNews = data.filter((news) => news.category_id == id);
+
+      //   console.log(filteredNews);
+
+      setCategoryNews(filteredNews);
+    }
+  }, [data, id]);
+
+  return <div>Total {categoryNews.length} news Found</div>;
 };
 
 export default CategoryNews;
