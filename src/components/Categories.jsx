fa37bd134ba7e02d25c5be3ae26870d369a75bf2
Antor-Chowdhury <antor1347@gmail.com>
2025-11-08 21:05:17 +0600

    added category

---
+++ src/components/Categories.jsx
@@ -0,0 +1,20 @@
+import React, { use } from "react";
+
+const categoryPromise = fetch("/categories.json").then((res) => res.json());
+
+const Categories = () => {
+  //   console.log(categoryPromise);
+
+  const categories = use(categoryPromise);
+  //   console.log(categories);
+
+  return (
+    <div>
+      <h2 className="font-semibold text-xl text-accent-2">
+        All Category ({categories.length})
+      </h2>
+    </div>
+  );
+};
+
+export default Categories;
