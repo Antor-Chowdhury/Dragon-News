6047e590d767966a2400e7120f5a4cddbad9ffe8
Antor-Chowdhury <antor1347@gmail.com>
2025-11-10 21:06:25 +0600

    added stikcy to homelayout

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -18,14 +18,14 @@ const HomeLayout = () => {
           <Navbar></Navbar>
         </nav>
       </header>
-      <main className="w-11/12 mx-auto mt-20 grid grid-cols-12 gap-6">
-        <aside className="col-span-3">
+      <main className="w-11/12 mx-auto mt-20 grid grid-cols-12 gap-6 mb-5">
+        <aside className="col-span-3 sticky top-0 h-fit">
           <LeftAside></LeftAside>
         </aside>
         <section className="main col-span-6">
           <Outlet></Outlet>
         </section>
-        <aside className="col-span-3">
+        <aside className="col-span-3 sticky top-0 h-fit mb-1">
           <RightAside></RightAside>
         </aside>
       </main>
