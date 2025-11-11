b05e1b28e11e75c799a57d149452c566519bca4e
Antor-Chowdhury <antor1347@gmail.com>
2025-11-11 21:26:20 +0600

    added firebase email intigation with register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,38 +1,81 @@
-import React from "react";
+import React, { use } from "react";
 import { Link } from "react-router";
+import { AuthContext } from "../provider/AuthProvider";
 
 const Register = () => {
+  const { createUser, setUser } = use(AuthContext);
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    // console.log(e.target);
+
+    const name = e.target.name.value;
+    const photo = e.target.photo.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(name, email, photo, password);
+
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        // console.log(result.user);
+        setUser(user);
+
+        // clear the form
+        e.target.reset();
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
   return (
     <div className="flex justify-center items-center mt-30 -ml-40">
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5 px-4">
         <h2 className="font-semibold text-3xl text-center">
           Login your account
         </h2>
-        <div className="card-body">
+        <form onSubmit={handleRegister} className="card-body">
           <fieldset className="fieldset">
             {/* Your Name */}
             <label className="label">Your Name</label>
             <input
+              name="name"
               type="text"
               className="input"
               placeholder="Enter your name"
+              required
             />
 
             {/* Photo URL */}
             <label className="label">Photo URL</label>
             <input
+              name="photo"
               type="text"
               className="input"
               placeholder="Enter your photo url"
+              required
             />
 
             {/* Email */}
             <label className="label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
+            <input
+              name="email"
+              type="email"
+              className="input"
+              placeholder="Email"
+              required
+            />
 
             {/* Password */}
             <label className="label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
+            <input
+              name="password"
+              type="password"
+              className="input"
+              placeholder="Password"
+              required
+            />
 
             {/* terms & conditions */}
             <p className="flex items-center gap-2 mt-2 text-accent">
@@ -40,7 +83,9 @@ const Register = () => {
               Accept <span className="font-medium">Terms & Conditions</span>
             </p>
 
-            <button className="btn btn-neutral mt-4">Register</button>
+            <button type="submit" className="btn btn-neutral mt-4">
+              Register
+            </button>
           </fieldset>
           <p className="text-center mt-3 font-semibold">
             Already Have An Account ?{" "}
@@ -49,7 +94,7 @@ const Register = () => {
               Login
             </Link>
           </p>
-        </div>
+        </form>
       </div>
     </div>
   );
