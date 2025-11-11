469e876f434605e207f23d78950512cb39fb736b
Antor-Chowdhury <antor1347@gmail.com>
2025-11-11 21:29:03 +0600

    added firebase demo config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB7s41z2YFSVN8w2u1qQQ8bXzhnUGbvDiA",
-  authDomain: "fir-news-11d3c.firebaseapp.com",
-  projectId: "fir-news-11d3c",
-  storageBucket: "fir-news-11d3c.firebasestorage.app",
-  messagingSenderId: "631049447897",
-  appId: "1:631049447897:web:616a693f901982279511c8",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
