3db97d9d756da10864b56f478120767c94c922a3
Antor-Chowdhury <antor1347@gmail.com>
2025-11-12 14:14:49 +0600

    added privateRoute

---
+++ src/provider/PrivateRoute.jsx
@@ -0,0 +1,24 @@
+import React, { use } from "react";
+import { AuthContext } from "./AuthProvider";
+import { Navigate } from "react-router";
+import Loading from "../pages/Loading";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = use(AuthContext);
+  //   console.log(user);
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
+
+  // if --> user thake return children
+  //   otherwise navigate to --> Login page
+
+  if (user && user?.email) {
+    return children;
+  } else {
+    return <Navigate to="/auth/login"></Navigate>;
+  }
+};
+
+export default PrivateRoute;
